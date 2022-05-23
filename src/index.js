2db9f3e3695911d23cae15a2eb7124b0df17439b
wanyoung <dhks2869@gmail.com>
2022-05-23 16:51:04 +0900

    ADD: 컨벤션 추가 및 초기설정 완료

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles/common.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>디너 플레이트</h1>
     <App />
   </React.StrictMode>
 );
