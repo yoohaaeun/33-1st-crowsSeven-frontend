ffa2b88ac063116385ebfa4aa7105c44f3c2eaa2
wanyoung <dhks2869@gmail.com>
2022-05-24 14:46:20 +0900

    ADD:Search 페이지 추가, MODIFY:파스칼케이스 카멜케이스로 변경

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,6 +6,18 @@ import { GrClose } from 'react-icons/gr';
 import { useState } from 'react';
 import './Nav.scss';
 
+const SearchBox = ({ handleSearchClick }) => {
+  return (
+    <>
+      <GrClose onClick={handleSearchClick} className="searchClose" />
+      <section className="searchBox">
+        <input type="text" placeholder="SEARCH" />
+        <span>검색어 입력후에 엔터를 누르세요.</span>
+      </section>
+    </>
+  );
+};
+
 const Logo = () => {
   return (
     <ul className="logo">
@@ -22,7 +34,7 @@ const Logo = () => {
 const RightBoxNav = ({ isToggle, handleClick }) => {
   return (
     <div className={isToggle ? 'rightNavBox widthZero' : 'rightNavBox'}>
-      <GrClose onClick={handleClick} className="xBox" />
+      <GrClose onClick={handleClick} className="cancel" />
       <ul className="rightBoxText">
         <li>로그인</li>
         <li>장바구니</li>
@@ -37,62 +49,71 @@ const RightBoxNav = ({ isToggle, handleClick }) => {
 
 const Nav = () => {
   const [isToggle, setToggle] = useState(true);
+  const [isSearch, setIsSearch] = useState(false);
   const handleClick = () => {
     setToggle(!isToggle);
   };
+  const handleSearchClick = () => {
+    setIsSearch(!isSearch);
+  };
   return (
-    <nav className="nav">
-      <ul className="navText">
-        <Logo />
-        <li className="navTextList">
-          ABOUT
-          <div className="navTextListInfo">
-            <li>STORE</li>
-            <li>COMPANY</li>
-            <li>CONTACT</li>
-            <li>STOCKIST</li>
-          </div>
-        </li>
-
-        <li className="navTextList">
-          STORE
-          <div className="navTextListInfo">
-            <li>RE-STOCK</li>
-            <li>카탈리나</li>
-            <li>컵</li>
-            <li>플레이트</li>
-            <li>보울</li>
-            <li>키친웨어</li>
-            <li>굿즈</li>
-          </div>
-        </li>
-        <li className="navTextList">
-          JOURNAL
-          <div className="navTextListInfo">
-            <li>MAGAZINE</li>
-          </div>
-        </li>
-        <li className="navTextList">
-          COMMUNITY
-          <div className="navTextListInfo">
-            <li>NOTICE</li>
-            <li>Q&A</li>
-            <li>REVIEW</li>
-            <div className="navTextListIcon">
-              <BsInstagram className="insta" />
-              <BsYoutube className="youtube" />
+    <>
+      <nav className="nav">
+        <ul className="navText">
+          <Logo />
+          <li className="navTextList">
+            ABOUT
+            <div className="navTextListInfo">
+              <li>STORE</li>
+              <li>COMPANY</li>
+              <li>CONTACT</li>
+              <li>STOCKIST</li>
             </div>
-          </div>
-        </li>
-        <li className="navTextList">WHOLESALE</li>
-      </ul>
-      <div className="navIcon">
-        <GoSearch />
-        <BsHandbag />
-        <GiHamburgerMenu onClick={handleClick} />
-      </div>
-      <RightBoxNav isToggle={isToggle} handleClick={handleClick} />
-    </nav>
+          </li>
+
+          <li className="navTextList">
+            STORE
+            <div className="navTextListInfo">
+              <li>RE-STOCK</li>
+              <li>카탈리나</li>
+              <li>컵</li>
+              <li>플레이트</li>
+              <li>보울</li>
+              <li>키친웨어</li>
+              <li>굿즈</li>
+            </div>
+          </li>
+          <li className="navTextList">
+            JOURNAL
+            <div className="navTextListInfo">
+              <li>MAGAZINE</li>
+            </div>
+          </li>
+          <li className="navTextList">
+            COMMUNITY
+            <div className="navTextListInfo">
+              <li>NOTICE</li>
+              <li>Q&A</li>
+              <li>REVIEW</li>
+              <div className="navTextListIcon">
+                <BsInstagram className="insta" />
+                <BsYoutube className="youtube" />
+              </div>
+            </div>
+          </li>
+          <li className="navTextList">WHOLESALE</li>
+        </ul>
+        <div className="navIcon">
+          <GoSearch onClick={handleSearchClick} />
+          <BsHandbag />
+          <GiHamburgerMenu onClick={handleClick} />
+        </div>
+        <RightBoxNav isToggle={isToggle} handleClick={handleClick} />
+      </nav>
+      {isSearch && (
+        <SearchBox isSearch={isSearch} handleSearchClick={handleSearchClick} />
+      )}
+    </>
   );
 };
 
