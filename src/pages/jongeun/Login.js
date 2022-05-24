f3cf35ade98f8d47bea95bc8ba64aecfbb05dfd3
Yumjong <yumjongeun@gmail.com>
2022-05-24 20:12:37 +0900

    [33기 염종은] ADD:Login page 기능구현

--- src/pages/jongeun/Login.js
+++ src/pages/jongeun/Login.js
@@ -1,22 +1,69 @@
-import React from 'react';
+import { React, useState } from 'react';
 import './Login.scss';
 
 const Login = () => {
+  const [inputValue, setInputValue] = useState({
+    id: '',
+    pw: '',
+  });
+
+  const { id, pw } = inputValue;
+
+  const handleInput = e => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const goToMain = () => {
+    fetch('http://10.58.3.110:8000/users/signin', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: id,
+        password: pw,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        if (result.token) {
+          localStorage.setItem('token', result.token);
+        }
+      });
+  };
+
   return (
     <div>
       <section className="loginArea">
-        <div className="loginTitle">CROWS SENVEN</div>
+        <div className="loginTitle">CROWS SEVEN</div>
         <div className="inputArea">
           <div className="idLine">
             <span className="idInputLine">Id</span>
-            <input className="idInput" />
+            <input
+              className="idInput"
+              type="text"
+              name="id"
+              onChange={handleInput}
+            />
           </div>
           <div className="pwLine">
             <span className="pwInputLine">Password</span>
-            <input type="password" className="pwInput" />
+            <input
+              className="pwInput"
+              type="password"
+              name="pw"
+              onChange={handleInput}
+            />
           </div>
         </div>
-        <button className="signBtn">SIGN IN</button>
+        <button
+          className="signBtn"
+          onClick={goToMain}
+          disabled={id.includes('@') && pw.length >= 5 ? false : true}
+        >
+          SIGN IN
+        </button>
         <div className="loginFooter">
           <div className="findUser">
             <div className="forgotId">Forgot Your Id?</div>
