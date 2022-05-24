ec113b3ed2b92f52a55552495ecc5e6f73bbd9ed
wanyoung <dhks2869@gmail.com>
2022-05-24 21:29:57 +0900

    ADD: 배경화면 setInterval추가

---
+++ src/pages/main/Img.js
@@ -0,0 +1,6 @@
+import './Img.scss';
+
+const Img = ({ src }) => {
+  return <img className="mainImg" src={src} alt="메인페이지 접시" />;
+};
+export default Img;
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,24 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MainButton from './MainButton';
+import Img from './Img';
 import './Main.scss';
 
-const Img = ({ src }) => {
-  return <img src={src} alt="메인페이지 접시" />;
-};
+let length = 0;
 
 const Main = () => {
-  const [imgData, setImgData] = useState([
-    {
-      id: 1,
-      url: '',
-    },
-  ]);
+  const [imgData, setImgData] = useState({
+    id: 1,
+    url: 'https://cdn.pixabay.com/photo/2019/02/25/08/43/cup-4019084_1280.jpg',
+  });
+  useEffect(() => {
+    setInterval(() => {
+      setImgData({
+        id: imgUrl[length].id,
+        url: imgUrl[length].url,
+      });
+      length += 1;
+      if (length >= 3) length = 0;
+    }, 3000);
+  }, []);
+
   return (
     <section className="mainSection">
-      <Img />
+      <Img className="renderTest" src={imgData.url} />
       <MainButton />
     </section>
   );
 };
+
+const imgUrl = [
+  {
+    id: 1,
+    url: 'https://cdn.pixabay.com/photo/2019/02/25/08/43/cup-4019084_1280.jpg',
+  },
+  {
+    id: 2,
+    url: 'https://cdn.pixabay.com/photo/2017/01/09/19/35/wedding-reception-1967373_1280.jpg',
+  },
+  {
+    id: 3,
+    url: 'https://cdn.pixabay.com/photo/2018/03/20/09/18/food-3242588_1280.jpg',
+  },
+];
 
 export default Main;
