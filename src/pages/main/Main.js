b2210e5d4149a2b8dcf0ff4dbfa6a7f85c9e1100
wanyoung <dhks2869@gmail.com>
2022-05-25 00:43:46 +0900

    ADD:slide dot

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
+import { BsDot } from 'react-icons/bs';
 import Footer from './Footer';
 import MainButton from './MainButton';
 import Img from './Img';
 import './Main.scss';
 
 let length = 0;
-
 const Main = () => {
   const [imgData, setImgData] = useState({
     id: 1,
     url: 'https://cdn.pixabay.com/photo/2019/02/25/08/43/cup-4019084_1280.jpg',
   });
+
   useEffect(() => {
     setInterval(() => {
       setImgData({
@@ -19,7 +20,7 @@ const Main = () => {
       });
       length += 1;
       if (length >= 3) length = 0;
-    }, 3000);
+    }, 5000);
   }, []);
 
   return (
@@ -27,6 +28,11 @@ const Main = () => {
       <section className="mainSection">
         <Img className="renderTest" src={imgData.url} />
         <MainButton />
+        <div className="slideDot">
+          <BsDot />
+          <BsDot />
+          <BsDot />
+        </div>
       </section>
       <Footer />
     </>
@@ -35,16 +41,16 @@ const Main = () => {
 
 const imgUrl = [
   {
-    id: 1,
-    url: 'https://cdn.pixabay.com/photo/2019/02/25/08/43/cup-4019084_1280.jpg',
-  },
-  {
     id: 2,
     url: 'https://cdn.pixabay.com/photo/2017/01/09/19/35/wedding-reception-1967373_1280.jpg',
   },
   {
     id: 3,
     url: 'https://cdn.pixabay.com/photo/2018/03/20/09/18/food-3242588_1280.jpg',
+  },
+  {
+    id: 1,
+    url: 'https://cdn.pixabay.com/photo/2019/02/25/08/43/cup-4019084_1280.jpg',
   },
 ];
 
