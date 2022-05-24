f32f6a7f5b483543e155d91c5719e9354e1f5c19
wanyoung <dhks2869@gmail.com>
2022-05-24 16:35:04 +0900

    ADD:이미지 버튼 추가

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,15 +1,11 @@
+import MainButton from './MainButton';
 import './Main.scss';
-
-const MainButton = () => {
-  return <button className="mainButton">구매하기</button>;
-};
 
 const Main = () => {
   return (
     <section className="mainSection">
       <img
-        className="mainImage"
-        src="https://cdn.pixabay.com/photo/2019/05/04/14/31/ceramic-4178215_1280.jpg"
+        src="https://cdn.pixabay.com/photo/2021/02/06/19/31/pancakes-5989144_1280.jpg"
         alt="메인페이지 접시"
       />
       <MainButton />
