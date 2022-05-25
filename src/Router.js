0c0e087ad55914eeb1e61ea24dd257851a1a5dbc
Yumjong <yumjongeun@gmail.com>
2022-05-25 16:32:57 +0900

    [33기 염종은]ADD:ordercheck 페이지 layout

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
+import OrderCheck from './pages/OrderCheck/OrderCheck';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Nav />} />
+        <Route path="/order" element={<OrderCheck />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/OrderCheck/OrderCheck.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OrderCheck = () => {
+  return (
+    <div>
+      <main className="orderCheck">
+        <div>asfd</div>
+      </main>
+    </div>
+  );
+};
+
+export default OrderCheck;
