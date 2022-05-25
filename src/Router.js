3da12d554f8055c7e63bb83ab4efa9cce400121b
Yumjong <yumjongeun@gmail.com>
2022-05-25 17:34:07 +0900

    conflict 확인 수정

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav';
-import Signup from './pages/jongeun/Signup';
+import Nav from './components/Nav/Nav';
+import Signup from './pages/Signup/Signup';
 
 const Router = () => {
   return (
