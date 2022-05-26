3f7ce3dbe8e2c6b41d5725e7883790642cffe85c
DevSeulgi <dev.dinobox@gmail.com>
2022-05-26 15:00:54 +0900

    ADD : 1차 레이아웃 완료

--- src/pages/productDetail/ProductDetail.js
+++ src/pages/productDetail/ProductDetail.js
@@ -1,9 +1,15 @@
 import './ProductDetail.scss';
+import Nav from '../../components/Nav/Nav';
+import { FiHeart } from 'react-icons/fi';
+import { MdPhotoSizeSelectSmall } from 'react-icons/md';
+import { GrFacebook, GrTwitter } from 'react-icons/gr';
+import { IoMdArrowDropup, IoMdArrowDropdown } from 'react-icons/io';
 import { useState, useEffect } from 'react';
 
 const ProductDetail = () => {
   const [list, setList] = useState([]);
   const { itemDetail, itemName, description, price } = list;
+
   useEffect(() => {
     fetch('/Data/ITEM_LIST.json')
       .then(res => res.json())
@@ -14,11 +20,12 @@ const ProductDetail = () => {
 
   return (
     <div className="productDetail">
-      <section className="leftSide">
-        <img src={itemDetail} alt="img" />
+      <Nav />
+      <section className="imgSide">
+        <img src={itemDetail} alt="Product Detail" />
       </section>
-      <section className="rightSide">
-        <div className="sideDetail">
+      <section className="descriptionSide">
+        <div className="sideWrapper">
           <h2>{itemName}</h2>
           <div className="description">
             <ul>
@@ -29,28 +36,42 @@ const ProductDetail = () => {
             </ul>
             <div className="amountTab">
               {itemName}
-              <div>
+              <div className="inputAmount">
                 <input type="number" />
-                <button>+</button>
-                <button>-</button>
+                <div className="amountHandler">
+                  <button className="up">
+                    <IoMdArrowDropup className="arrow" />
+                  </button>
+                  <button className="down">
+                    <IoMdArrowDropdown className="arrow" />
+                  </button>
+                </div>
               </div>
             </div>
             <div className="totalPrice">
               TOTAL :<span>{price}</span> (1개)
             </div>
             <div className="buyBtn">
-              <button>구매하기</button>
-              <button>장바구니</button>
+              <button className="buy">구매하기</button>
+              <button className="cart">장바구니</button>
             </div>
-            <button>WISH LIST</button>
+            <button className="wishList">
+              <FiHeart />
+              <span>WISH LIST</span>
+            </button>
             <div className="aboutItemBtn">
-              <button>상품후기 (5)</button>
-              <button>상품문의 (0)</button>
-              <button>SIZE</button>
+              <span>
+                <button>상품후기 (5)</button>
+                <button>상품문의 (0)</button>
+              </span>
+              <button className="size">
+                <MdPhotoSizeSelectSmall className="sizeImg" />
+                SIZE
+              </button>
             </div>
             <div className="snsBtn">
-              <button>facebook</button>
-              <button>twitter</button>
+              <GrFacebook className="facebookBtn" />
+              <GrTwitter className="twitterBtn" />
             </div>
           </div>
         </div>
