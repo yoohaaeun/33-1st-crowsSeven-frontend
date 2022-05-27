62d8ba1e3373994113661e76b66be9444a846d6a
wanyoung <dhks2869@gmail.com>
2022-05-27 11:47:17 +0900

    ADD:캐러셀 카드 컴포넌트 및 캐러셀 추가

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Signup from './pages/Signup/Signup';
+import Carousel from './pages/main/Carousel';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Nav />} />
+        <Route path="/carousel" element={<Carousel />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
