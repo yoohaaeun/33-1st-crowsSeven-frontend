1700c399d629c99c80e0f501659f5fb3204a2b5e
sourmix <75124027+sourmix@users.noreply.github.com>
2022-05-27 18:41:45 +0900

    sider 수정

--- src/review/ReviewPage.js
+++ src/review/ReviewPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Nav from '../../src/components/Nav/Nav';
 import TextItem from '../components/review/TextItem';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
