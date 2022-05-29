d5d9c76b5e05df52ba97a6e2a9275a7917008e67
DevSeulgi <dev.dinobox@gmail.com>
2022-05-30 07:54:36 +0900

    Modify : Conflict 해결

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
-import Router from './Router.js';
-import ReactDOM from 'react-dom/client';
-import './styles/reset.scss';
-import './styles/common.scss';
-=======
 import ReactDOM from 'react-dom/client';
 import Router from './Router.js';
 import './styles/common.scss';
 import './styles/reset.scss';
->>>>>>> origin
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
