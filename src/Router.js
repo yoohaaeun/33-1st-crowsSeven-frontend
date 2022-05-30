26957c7c68755fc5ebfb517f96ac4de58987b84b
DevSeulgi <dev.dinobox@gmail.com>
2022-05-30 16:46:07 +0900

    Add : 레이아웃 추가

--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,7 @@ import Nav from './components/Nav/Nav';
 import Post from './review/Post';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
+import StoreModal from './components/storeModal/StoreModal';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Nav />} />
         <Route path="/store" element={<Store />} />
+        <Route path="/storeModal" element={<StoreModal />} />
         <Route path="/productDetail" element={<ProductDetail />} />
         <Route path="/review_post" element={<Post />} />
         <Route path="/cart" element={<Cart />} />
--- src/components/storeModal/StoreModal.js
+++ src/components/storeModal/StoreModal.js
@@ -2,8 +2,58 @@ import './StoreModal.scss';
 
 const StoreModal = () => {
   return (
-    <div>
-      <h2>Quick Shop</h2>
+    <div className="storeModal">
+      <div className="modalBackground" />
+      <div className="modalComponent">
+        <section className="imgSection">
+          <span>product thumbnail</span>
+        </section>
+        <section className="infoSection">
+          <h2>itemName</h2>
+          <hr />
+          <table>
+            <tr>
+              <th>상품 간략설명</th>
+              <td>description</td>
+            </tr>
+            <tr>
+              <th>판매가</th>
+              <td>price</td>
+            </tr>
+            <tr>
+              <th>배송 방법</th>
+              <td>택배</td>
+            </tr>
+            <tr>
+              <th>배송비</th>
+              <td>￦3,000 (￦50,000 이상 구매 시 무료)</td>
+            </tr>
+            <tr>
+              <th>SNS 상품홍보</th>
+              <td>
+                <button>facebook</button>
+                <button>twitter</button>
+              </td>
+            </tr>
+          </table>
+          <hr />
+          <div>(최소주문수량 1개 이상)</div>
+          <div>수량을 선택해주세요.</div>
+          <hr />
+          <div>
+            <span>itemName</span>
+            <input type="Number" />
+            <span>price</span>
+          </div>
+          <hr />
+          <div>총 상품금액(수량) : ￦ price ( 개)</div>
+          <div>
+            <button>BUY NOW</button>
+            <button>CART</button>
+          </div>
+          <div>+ WISH LIST</div>
+        </section>
+      </div>
     </div>
   );
 };
